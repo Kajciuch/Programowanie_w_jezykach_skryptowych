1b33c17b0c1ef09fa82cd7c3bb599d4edcfb9a17
Kaja Thiel <kajat224@gmail.com>
2025-06-16 01:36:19 +0200

    update

--- projekt_dev/src/components/Footer.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-export default () => <footer style={{ marginTop: '2em' }}>
-  <hr />
-  <p>© {new Date().getFullYear()} Twoje Imię. Wszelkie prawa zastrzeżone.</p>
-</footer>;
--- projekt_dev/src/components/ThemeToggle.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-import { ThemeContext } from '../App';
-
-export default function ThemeToggle() {
-  const { dark, setDark } = useContext(ThemeContext);
-  return <button onClick={() => setDark(!dark)}>
-    Przełącz na {dark ? 'jasny' : 'ciemny'} motyw
-  </button>;
-}
